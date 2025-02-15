378ac0afc89fa13648aab5775281e03fa67b540c
Allen <volt772@naver.com>
2025-02-15 15:41:26 +0900

    [*] MG-14, Change Hanhwa Eagles Stadium

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.apx8.mongoose"
         minSdk = 31
         targetSdk = 34
-        versionCode = 10005
-        versionName = "1.0.5"
+        versionCode = 10006
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
