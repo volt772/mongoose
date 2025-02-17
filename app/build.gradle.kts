f7ac5f9d56362bec3f33833e928851d9c69e702b
Allen <volt772@naver.com>
2025-02-17 21:16:40 +0900

    [*] MG-16, 디버깅모드인지 아닌지 화면 표시

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.apx8.mongoose"
         minSdk = 31
         targetSdk = 34
-        versionCode = 10006
-        versionName = "1.0.6"
+        versionCode = 10007
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
