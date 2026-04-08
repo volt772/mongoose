6012122c954fae484209d3c37130bb193b78a942
Allen <volt772@naver.com>
2026-04-08 20:20:11 +0900

    [*] Version Up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.apx8.mongoose"
         minSdk = 31
         targetSdk = 35
-        versionCode = 10110
-        versionName = "1.1.10"
+        versionCode = 10111
+        versionName = "1.1.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
